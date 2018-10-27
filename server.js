3cee0fa6f4b341bb36d20fc1d701e15e7cd1a50c
César Pérez <cesaruni84@gmail.com>
2018-10-26 21:13:45 -0500

    carga inicial2

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ path = require('path');
 
 // app.use(express.static('./dist/modfac-front-end'));
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/egret'));
+
 app.listen(process.env.PORT || 8080, () => {
     console.log('Server started');
 })
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/egret/index.html'));
 
 });
