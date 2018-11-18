0710ad60ef341dc32aef166c36c261a582b76713
César Pérez <cesaruni84@gmail.com>
2018-11-18 01:29:15 -0500

    update a backend prod aws 18/11/2018

--- src/app/shared/helpers/var.constant.ts
+++ src/app/shared/helpers/var.constant.ts
@@ -5,4 +5,8 @@
 // export const HOST = 'http://localhost:8080/v1/sisyfac/api';
 
 // Desarrollo - AWS Backend
-export const HOST = 'http://ec2-13-58-145-206.us-east-2.compute.amazonaws.com:8080/v1/sisyfac/api';
+// export const HOST = 'http://ec2-13-58-145-206.us-east-2.compute.amazonaws.com:8080/v1/sisyfac/api';
+
+
+// PRODUCCION - AWS Backend
+export const HOST = 'http://ec2-18-224-40-63.us-east-2.compute.amazonaws.com:8080/v1/sisyfac/api/';
