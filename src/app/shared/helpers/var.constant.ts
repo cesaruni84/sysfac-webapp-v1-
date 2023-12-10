ba754562d735a54221f5a85846f35345fcafd8ad
cesar <cesaruni84@gmail.com>
2023-12-10 16:43:22 -0500

    update gitignore and url for api sysfac

--- src/app/shared/helpers/var.constant.ts
+++ src/app/shared/helpers/var.constant.ts
@@ -10,7 +10,8 @@ export const GLOSA_TRANSPORTE = 'TRANSPORTE';
 // PRODUCCION - AWS Backend - BD AWS
 // export const HOST = 'http://facturacion-sysfac.us-east-2.elasticbeanstalk.com:8080//v1/sisyfac/api';
 
-// PRODUCCION - AWS Backend - BD AWS V2
-export const HOST = 'http://ec2-3-135-153-239.us-east-2.compute.amazonaws.com:8080/v1/sisyfac/api';
+// PRODUCCION - AWS Backend - BD AWS V2 - Heroku
+// export const HOST = 'http://ec2-3-135-153-239.us-east-2.compute.amazonaws.com:8080/v1/sisyfac/api';
 
-
+// PRODUCCION - AWS Backend - BD AWS V4 - Firebase
+export const HOST = 'https://api-sysfac-env.eba-rm8nkxm6.us-east-2.elasticbeanstalk.com:5000/v1/sisyfac/api';
