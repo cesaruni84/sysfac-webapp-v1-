c3cc41dc03e9742757a9e835a152d47f4abe16e7
cesar <cesaruni84@gmail.com>
2024-02-20 12:14:59 -0500

    feat: update api connection

--- src/app/shared/helpers/var.constant.ts
+++ src/app/shared/helpers/var.constant.ts
@@ -15,3 +15,6 @@ export const GLOSA_TRANSPORTE = 'TRANSPORTE';
 
 // PRODUCCION - AWS Backend - BD AWS V4 - Firebase
 export const HOST = 'https://api-sysfac-env.eba-rm8nkxm6.us-east-2.elasticbeanstalk.com:5000/v1/sisyfac/api';
+
+// PRODUCCION - Railway Backend - API Facturacion
+//export const HOST = 'https://api-facturacion-production.up.railway.app/v1/sisyfac/api';
