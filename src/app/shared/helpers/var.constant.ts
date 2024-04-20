828ffe3ccbcca597995e0a52aae96c3bd17d41ea
cesar <cesaruni84@gmail.com>
2024-04-20 18:27:14 -0500

    feat: new provider for api sysfac-v1

--- src/app/shared/helpers/var.constant.ts
+++ src/app/shared/helpers/var.constant.ts
@@ -1,20 +1,11 @@
 // tslint:disable-next-line:eofline
 export const GLOSA_TRANSPORTE = 'TRANSPORTE';
 
-// Desarrollo - Local Backend - BD AWSng
-// export const HOST = 'http://localhost:8080/v1/sisyfac/api';
-
-// Desarrollo - AWS Backend - BD AWS
-// export const HOST = 'http://ec2-13-58-145-206.us-east-2.compute.amazonaws.com:8080/v1/sisyfac/api';
-
-// PRODUCCION - AWS Backend - BD AWS
-// export const HOST = 'http://facturacion-sysfac.us-east-2.elasticbeanstalk.com:8080//v1/sisyfac/api';
-
-// PRODUCCION - AWS Backend - BD AWS V2 - Heroku
-// export const HOST = 'http://ec2-3-135-153-239.us-east-2.compute.amazonaws.com:8080/v1/sisyfac/api';
+// Desarrollo - Local Backend - BD Local
+//export const HOST = 'http://localhost:8443/v1/sisyfac/api';
 
 // PRODUCCION - AWS Backend - BD AWS V4 - Firebase
-export const HOST = 'https://api-sysfac-env.eba-rm8nkxm6.us-east-2.elasticbeanstalk.com:5000/v1/sisyfac/api';
+// export const HOST = 'https://api-sysfac-env.eba-rm8nkxm6.us-east-2.elasticbeanstalk.com:5000/v1/sisyfac/api';
 
 // PRODUCCION - Railway Backend - API Facturacion
-//export const HOST = 'https://api-facturacion-production.up.railway.app/v1/sisyfac/api';
+export const HOST = 'https://sysfac-api-v1-production.up.railway.app/v1/sisyfac/api';
