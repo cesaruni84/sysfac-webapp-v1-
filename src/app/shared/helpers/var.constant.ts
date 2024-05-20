6595054858d55fb4b143c867fe0260f9df43fc09
cesar <cesaruni84@gmail.com>
2024-05-19 22:45:07 -0500

    feat: change url backend api service

--- src/app/shared/helpers/var.constant.ts
+++ src/app/shared/helpers/var.constant.ts
@@ -2,7 +2,7 @@
 export const GLOSA_TRANSPORTE = 'TRANSPORTE';
 
 // Desarrollo - Local Backend - BD Local
-//export const HOST = 'http://localhost:8443/v1/sisyfac/api';
+// export const HOST = 'http://localhost:8443/v1/sisyfac/api';
 
 // PRODUCCION - AWS Backend - BD AWS V4 - Firebase
 // export const HOST = 'https://api-sysfac-env.eba-rm8nkxm6.us-east-2.elasticbeanstalk.com:5000/v1/sisyfac/api';
